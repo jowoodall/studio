ad0a1d458bab012b4e64a40bab1f5f0f3714940d
Joey Woodall <joey.woodall@gmail.com>
2025-07-26 13:21:07 +0000

    fix dashboard width.

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -29,7 +29,7 @@ export default async function DashboardPage() {
       />
       
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mb-8">
-        <div className="lg:col-span-2">
+        <div className="lg:col-span-1">
             <MyNextRyd />
         </div>
         <div>
